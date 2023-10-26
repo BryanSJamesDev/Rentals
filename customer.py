d8fac679dcce060e4b1da463582be7cc0f43967c
Bryan Samuel James <65362097+bryanspacex@users.noreply.github.com>
2023-10-26 23:45:27 +0300

    Update customer.py

--- customer.py
+++ customer.py
@@ -18,3 +18,39 @@ def ADD():
         c_id = 'C1'
 
     name = input('Enter your name: ').upper()
+    while True:
+        try:
+            mob1 = int(input('Enter mobile 1: '))
+            if len(str(mob1)) != 8:
+                print('Mobile number should have 8 digits only')
+                continue
+            sql = 'Select * from customer where mobile_1 = %d' % mob1
+            mycursor.execute(sql)
+            if mycursor.rowcount:
+                print('Mobile Number already exists! Please try again')
+                continue
+        except:
+            print('Invalid Mobile Number entered! Please try again')
+            continue
+        break
+
+    while True:
+        try:
+            mob2 = int(input('Enter mobile 2: '))
+            if len(str(mob2)) != 8:
+                print('Mobile number should have 8 digits only')
+                continue
+        except:
+            print('Invalid Mobile Number entered! Please try again')
+            continue
+        break
+
+    while True:
+        email = input('Enter email address: ').lower()
+        sql = "select * from customer where email='%s'" % email
+        mycursor.execute(sql)
+        mycursor.fetchall()
+        if mycursor.rowcount:
+            print("This email already exists! Please try again!")
+            continue
+        break
