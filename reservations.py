5ebf4b36249b6620df7c7dbc0f143e937dc03c3b
Bryan Samuel James <65362097+bryanspacex@users.noreply.github.com>
2023-11-09 23:28:58 +0300

    Update reservations.py

--- reservations.py
+++ reservations.py
@@ -15,3 +15,23 @@ def overdue():
     cdict = dict(c_idlist)
     clist = list(cdict.keys())
     rlist = []
+
+    if len(clist):
+        sql = 'select c_id from customer where c_id IN %s and status = "A" order by length(c_id),c_id' % str(tuple(clist))
+        mycursor.execute(sql)
+
+        clist = mycursor.fetchall()
+        clist = [i[0] for i in clist]
+        temp = list(cdict.keys())
+        for i in temp:
+            if i not in clist:
+                del cdict[i]
+
+        for i in cdict:
+            sql = 'select r_id from reservations where c_id = "%s" and c_date = "%s" order by length(r_id), r_id' % (i, cdict[i])
+            mycursor.execute(sql)
+            a = mycursor.fetchone()[0]
+            rlist.append(a)
+
+    mycon.close()
+    return rlist, clist
