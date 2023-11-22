6ab52b6aeea1ce30d5a0c5daceca36974d9d6bba
Bryan Samuel James <65362097+bryanspacex@users.noreply.github.com>
2023-11-22 09:29:38 +0300

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,11 @@
+import customer_related_functions, reservation_related_functions, car_related_functions
+
+print('Welcome to Tess Rentals')
+print('\nTo exit the program at any point, hit CTRL + C')
+
+while True:
+    print('\nMENU')
+    print('1. CARS')
+    print('2. CUSTOMERS')
+    print('3. RESERVATIONS')
+    print('4. EXIT')
