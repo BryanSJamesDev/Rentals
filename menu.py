e242203cfafe02b89a5d97ce6cbf080bec74374e
Bryan Samuel James <65362097+bryanspacex@users.noreply.github.com>
2023-11-26 12:50:00 +0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -39,3 +39,29 @@ while True:
                 reservation_related_functions.close_reservation()
             else:
                 break
+
+    elif choice == '2':
+        while True:
+            print('\n1. ADD NEW CUSTOMER')
+            print('2. VIEW ONE CUSTOMER')
+            print('3. VIEW ALL CUSTOMERS')
+            print('4. UPDATE CUSTOMER DETAILS')
+            print('5. BACK')
+
+            while True:
+                choice1 = input('Enter your choice: ')
+                if not choice1.isdigit() or len(choice1) > 1 or choice1 not in '12345':
+                    print('Invalid Choice, Please try again')
+                else:
+                    break
+
+            if choice1 == '3':
+                customer_related_functions.VIEWALL()
+            elif choice1 == '1':
+                customer_related_functions.ADD()
+            elif choice1 == '4':
+                customer_related_functions.UPDATE()
+            elif choice1 == '5':
+                break
+            else:
+                customer_related_functions.VIEWONE()
